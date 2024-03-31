1969a0bdeef1ffbecb61260a2ca34c31307d81ab
User <User@DESKTOP-6QTP69L>
2024-03-31 18:23:15 +0630

    Add project files.

---
+++ Reverse/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your name: ");
+        string? name = Console.ReadLine();
+
+
+        char[] charArray = name.ToCharArray(); //change string to char
+
+        //Reverse 
+        ReverseCharArray(charArray);
+
+        Console.WriteLine("\nReversed array:");
+        PrintCharArray(charArray);
+    }
+
+    static void ReverseCharArray(char[] array)
+    {
+        int start = 0;
+        int j = array.Length - 1;
+
+        while (start < j)
+        {
+
+            char temp = array[start];
+            array[start] = array[j];
+            array[j] = temp;
+
+            start++;
+            j--;
+        }
+    }
+    static void PrintCharArray(char[] array) // accept reverse char in arr
+    {
+        foreach (char c in array)
+        {
+            Console.Write(c + " ");
+        }
+    }
+
+
+}
+
